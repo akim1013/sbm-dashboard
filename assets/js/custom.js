c8c1f7f470d7e7b206e11b921f7529dc8dac641c
laguna99999 <lookatme99999@yahoo.com>
2019-10-08 22:59:38 +1000

    Login user

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,35 @@
-$(document).ready(function(){
-    // Login User
-    $('#login-form').submit(function(e){
-        $('.loader').removeClass('hide');
-        e.stopPropagation();
-        e.preventDefault();
-        $.ajax({
-            url: '/auth/login',
-            method: 'post',
-            data: $(this).serialize(),
-            success: function(res){
-                $('.loader').addClass('hide');
-                var response = JSON.parse(res);
-                if(response.status == 'failed'){
-                    $.toast({
-                        heading: 'Error',
-                        text: response.msg,
-                        showHideTransition: 'fade',
-                        icon: 'error',
-                        position: 'top-right'
-                    })
-                }else{
-                    $.toast({
-                        heading: 'Success',
-                        text: response.msg,
-                        showHideTransition: 'slide',
-                        icon: 'success',
-                        position: 'top-right'
-                    })
-                    setTimeout(function(){
-                        localStorage.setItem('shop_name', response.shop_name);
-                        window.location.assign('/');
-                    }, 3500);
-                }
+$('#login-form').submit(function(e){
+    $('.loader').removeClass('hide');
+    e.stopPropagation();
+    e.preventDefault();
+    $.ajax({
+        url: '/auth/login',
+        method: 'post',
+        data: $(this).serialize(),
+        success: function(res){
+            $('.loader').addClass('hide');
+            var response = JSON.parse(res);
+            if(response.status == 'failed'){
+                $.toast({
+                    heading: 'Error',
+                    text: response.msg,
+                    showHideTransition: 'fade',
+                    icon: 'error',
+                    position: 'top-right'
+                })
+            }else{
+                $.toast({
+                    heading: 'Success',
+                    text: response.msg,
+                    showHideTransition: 'slide',
+                    icon: 'success',
+                    position: 'top-right'
+                })
+                setTimeout(function(){
+                    localStorage.setItem('shop_name', response.shop_name);
+                    window.location.assign('/');
+                }, 3500);
             }
-        });
-    })
+        }
+    });
 })
