fff9aa251af33603fe8a5be99da3c513195c75c3
laguna99999 <lookatme99999@yahoo.com>
2019-10-14 03:00:03 +1000

    Language config, hook add

---
+++ application/language/chinese/sbm_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['auth_username']      = '用戶名';
+$lang['auth_password']      = '密碼';
+$lang['auth_login']         = '登錄';
+
+$lang['auth_heading']       = 'SBM儀表板登錄';
+$lang['auth_subtitle_1']    = '目標是將數據轉化為信息,';
+$lang['auth_subtitle_2']    = '和信息洞察力';
---
+++ application/language/english/sbm_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['auth_username']      = 'Username';
+$lang['auth_password']      = 'Password';
+$lang['auth_login']         = 'Login';
+
+$lang['auth_heading']       = 'SBM Dashboard Login';
+$lang['auth_subtitle_1']    = 'The goal is to turn data into information,';
+$lang['auth_subtitle_2']    = 'and information into insight';
