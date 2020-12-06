fdae5e21ee1b9c9dfc7fd870fb4fae5bcbf7be33
laguna1013 <lookatme99999@yahoo.com>
2020-12-06 19:19:48 +1000

    package.json added, express, path package added

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// // Serve static files....
+// app.use(express.static(__dirname + '/'));
+//
+// // Send all requests to index.html
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/purchasing-system/index.html'));
+// });
+
 // default Heroku PORT
 app.listen(process.env.PORT || 3000, function(){
-    console.log('App is running on port 3000')
+    console.log('App is running on http://localhost:3000')
 });
