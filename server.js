29f253f3a0cedb7feeaa9801927262f1031ff16c
smetandima <iltw009@gmail.com>
2020-12-07 06:47:12 +1000

    .env added

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
+const sql = require("mssql")
+
+const router = express.Router()
+
+require('dotenv').config()
+//const utiles = require('ns/utiles')
+
+const CONFIG = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  server: process.env.DB_HOST,
+  database: 'meetfresh',
+  options: {
+    enableArithAbort: true
+  }
+};
+
+let runQuery = (query) => {
+
+}
 
 app.get('/api', function(req, res) {
-  const sql = require("mssql");
-  const config = {
-    user: 'laguna',
-    password: 'goqkdtks.1234',
-    server: 'localhost',
-    database: 'meetfresh'
-  };
-  sql.connect(config, function (err) {
+  sql.connect(CONFIG).then(() => {
+    //return sql.query`select * from shops`
+  }).then(result => {
+    res.send(`Connection established successfully`)
+  }).catch(err => {
+    res.send(`Connection is not established.`)
+  })
 
-    if (err) console.log(err);
+  sql.on('error', err => {
 
-    // create Request object
-    var request = new sql.Request();
-
-    // query to the database and get the records
-    request.query('select * from shops', function (err, recordset) {
-
-        if (err) console.log(err)
-
-        // send records as a response
-        res.send(recordset);
-
-    });
-  });
+  })
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000, function(){
-    console.log('App is running on http://localhost:3000')
+app.listen(process.env.PORT || 3001, function(){
+    console.log('App is running on http://localhost:3001')
 });
