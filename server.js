f1c703c158fa41d73e299069aa8853ec4d899fd8
laguna1013 <lookatme99999@yahoo.com>
2020-12-06 21:13:54 +1000

    Connnect MSSQL

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// // Serve static files....
-// app.use(express.static(__dirname + '/'));
-//
-// // Send all requests to index.html
 app.get('/api', function(req, res) {
-    res.send('Nodejs API server is live');
+  const sql = require("mssql");
+  const config = {
+    user: 'laguna',
+    password: 'goqkdtks.1234',
+    server: 'localhost',
+    database: 'meetfresh'
+  };
+  sql.connect(config, function (err) {
+
+    if (err) console.log(err);
+
+    // create Request object
+    var request = new sql.Request();
+
+    // query to the database and get the records
+    request.query('select * from shops', function (err, recordset) {
+
+        if (err) console.log(err)
+
+        // send records as a response
+        res.send(recordset);
+
+    });
+  });
 });
 
 // default Heroku PORT
