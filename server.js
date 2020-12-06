cc20eeccd0fef572f84a7cb333fca3ae192a8f7d
laguna1013 <lookatme99999@yahoo.com>
2020-12-06 21:06:08 +1000

    node server test2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // app.use(express.static(__dirname + '/'));
 //
 // // Send all requests to index.html
-app.get('/', function(req, res) {
+app.get('/api', function(req, res) {
     res.send('Nodejs API server is live');
 });
 
